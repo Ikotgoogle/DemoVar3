09948b00d773e590e047ae84054235d48cb38dd4
Evan <ikotskiy@gmail.com>
2024-11-17 01:36:22 +0700

    Сделай регистрацию (создание пользователя) и чо то там еще надо было заебало оно уже

---
+++ Model/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoVar3.Model {
+    public class MyContext : DbContext {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<FavouriteList> Favorites { get; set; }
+        public DbSet<BooksInOrder> BooksInOrders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=DemoVar3DB1;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Model/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoVar3.Model {
+    public class Order {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public DateOnly OrderDate { get; set; }
+        public DateOnly DeliveryDate { get; set; }
+        public ObservableCollection<BooksInOrder> BooksInOrder { get; set; }
+    }
+}
